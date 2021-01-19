2c4c774790b91e2760fd5c6accfe25c34c383abf
Sanyam <64831699+sanyam01@users.noreply.github.com>
2021-01-18 19:43:17 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+// src/App.js
+import React from 'react';
+
+import { API, graphqlOperation } from 'aws-amplify'
+// import uuid to create a unique client ID
+//import uuid4 from 'v4'
+//import {v4 as uuidv4} from 'uuid';
+//import uuid from 'uuid/v4';
+//import {v4 as uuidv4} from 'uuid';
+//import uuid from 'uuid/v4';
+import {v1 as uuid} from "uuid"; 
+import { listTalks as ListTalks } from './graphql/queries'
+// import the mutation
+import { createTalk as CreateTalk } from './graphql/mutations'
+
+const CLIENT_ID = uuid()
+
+class App extends React.Component {
+  // define some state to hold the data returned from the API
+  state = {
+    name: '', description: '', speakerName: '', speakerBio: '', talks: []
+  }
+
+  // execute the query in componentDidMount
+  async componentDidMount() {
+    try {
+      const talkData = await API.graphql(graphqlOperation(ListTalks))
+      console.log('talkData:', talkData)
+      this.setState({
+        talks: talkData.data.listTalks.items
+      })
+    } catch (err) {
+      console.log('error fetching talks...', err)
+    }
+  }
+  createTalk = async() => {
+    const { name, description, speakerBio, speakerName } = this.state
+    if (name === '' || description === '' || speakerBio === '' || speakerName === '') return
+
+    const talk = { name, description, speakerBio, speakerName, clientId: CLIENT_ID }
+    const talks = [...this.state.talks, talk]
+    this.setState({
+      talks, name: '', description: '', speakerName: '', speakerBio: ''
+    })
+
+    try {
+      await API.graphql(graphqlOperation(CreateTalk, { input: talk }))
+      console.log('item created!')
+    } catch (err) {
+      console.log('error creating talk...', err)
+    }
+  }
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  render() {
+    return (
+      <>
+        <input
+          name='name'
+          onChange={this.onChange}
+          value={this.state.name}
+          placeholder='name'
+        />
+        <input
+          name='description'
+          onChange={this.onChange}
+          value={this.state.description}
+          placeholder='description'
+        />
+        <input
+          name='speakerName'
+          onChange={this.onChange}
+          value={this.state.speakerName}
+          placeholder='speakerName'
+        />
+        <input
+          name='speakerBio'
+          onChange={this.onChange}
+          value={this.state.speakerBio}
+          placeholder='speakerBio'
+        />
+        <button onClick={this.createTalk}>Create Talk</button>
+        {
+          this.state.talks.map((talk, index) => (
+            <div key={index}>
+              <h3>{talk.speakerName}</h3>
+              <h5>{talk.name}</h5>
+              <p>{talk.description}</p>
+            </div>
+          ))
+        }
+      </>
+    )
+  }
+}
+
+export default App
